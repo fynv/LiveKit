0f9f9516df2681099349e9a8aa933053bc8824b5
feiy <feiy@nvidia.com>
2021-02-24 21:49:43 +0800

    Adding test_player.

---
+++ python/test/test_player.py
@@ -0,0 +1,15 @@
+import sys
+import LiveKit as lk
+
+if len(sys.argv)>1:
+    port = lk.VideoPort()
+    player = lk.Player(sys.argv[1])
+    player.add_target(port)
+    width, height = player.video_size()
+    width = int(width*1080/height)
+    height = 1080
+    scene = lk.Compositor(width, height, width, height, "Playing: "+sys.argv[1])
+    scene.set_source(0, port)
+    player.start()   
+    while scene.draw():
+        pass

1e3c7dc6b4fbd08ed2497954814dc219f771f4cb
feiy <feiy@nvidia.com>
2021-02-26 18:41:53 +0800

    Minor fix.

--- python/test/test_player.py
+++ python/test/test_player.py
@@ -2,8 +2,9 @@ import sys
 import LiveKit as lk
 
 if len(sys.argv)>1:
+    lst_out = lk.AudioOutputDeviceList()
     port = lk.VideoPort()
-    player = lk.Player(sys.argv[1])
+    player = lk.Player(sys.argv[1], True, True, lst_out.id_default)
     player.add_target(port)
     width, height = player.video_size()
     width = int(width*1080/height)

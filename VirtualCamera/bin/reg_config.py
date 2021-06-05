c7918aca0761133547e39b568748fccae8315c71
feiy <feiy@nvidia.com>
2021-06-05 08:12:38 +0800

    virtual camera fix

--- VirtualCamera/bin/reg_config.py
+++ VirtualCamera/bin/reg_config.py
@@ -1,8 +1,8 @@
 import winreg
 
 MAPPING_NAME = "LiveKitVCam"
-VIDEO_WIDTH = 640
-VIDEO_HEIGHT = 480
+VIDEO_WIDTH = 1200
+VIDEO_HEIGHT = 900
 
 key = winreg.CreateKey(winreg.HKEY_CURRENT_USER, "Software\\LiveKitVCam\\Video")
 winreg.SetValueEx(key, "mapping_name", 0, winreg.REG_SZ, MAPPING_NAME)
